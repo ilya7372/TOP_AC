804889af4a882cfc099483626a462531729bd0bb
ilya7372 <iluha23699@gmail.com>
2024-05-14 21:28:43 +1000

    Я Крутой

---
+++ leson1/script.js
@@ -0,0 +1,36 @@
+// let massege;
+
+// massege = 2
+
+// console.log(massege)
+
+// let one = 1
+// let two = 2
+// let sum = one + two
+
+// console.log(one)
+// console.log(two)
+// console.log(sum)
+
+// let basket = 10
+// let basket2=basket
+
+// let name = prompt("Как тебя зовут?")
+// let massege = ` Првиет, ${name}`
+// alert(massege)
+
+// let number1=10
+// let number2=10.30
+
+// string - строка
+// Number - число
+
+// BigInt
+
+// Boolean - правда
+// // // + - * ** / < > || &&
+// let a = true
+// let b = false
+
+// let age = null;
+// let age2 = undefiend
